69364509bf7bcfca7d8a2d794ae3172f60cf70d2
Isaac M Bazian <bazia001@umn.edu>
2023-03-30 10:11:06 -0500

    Subset tibble based on 75% or more missingness in variables. Probably a cleaner way to do this, preferably in one pipe, but this works for now

--- R/week10.R
+++ R/week10.R
@@ -4,4 +4,12 @@ library(tidyverse)
 library(haven) #I think this is meant to be in tidyverse, but the functions didn't show up unless I called it directly
 
 ## Data Import and Cleaning
-gss_tbl <- read_sav(file = "../data/GSS2016.sav")
+#This code reads in the data and removes cases where HRS1 is NA
+gss_tbl_original <- read_sav(file = "../data/GSS2016.sav") %>% 
+  filter(!is.na(HRS1))
+
+#This code identifies the variables in the previous tibble with 75% or more missingness
+missing_75 <- colMeans(is.na(gss_tbl_original)) >= .75
+
+#This code subsets the tibble to include only those columns with less than 75% missingness
+gss_tbl <- gss_tbl_original[,!missing_75]
