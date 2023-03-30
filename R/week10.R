656105da2d059911ede29e64457a49694de98233
Isaac M Bazian <bazia001@umn.edu>
2023-03-30 09:23:57 -0500

    Read in initial data with haven package

--- R/week10.R
+++ R/week10.R
@@ -1,5 +1,7 @@
 ## Script Settings and Resources
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(tidyverse)
+library(haven) #I think this is meant to be in tidyverse, but the functions didn't show up unless I called it directly
 
-
+## Data Import and Cleaning
+gss_tbl <- read_sav(file = "../data/GSS2016.sav")
